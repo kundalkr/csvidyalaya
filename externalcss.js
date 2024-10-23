ec1d72e44048cc191810c4c40e7d0cb3ca9b4ec3
kundal <kundal5335@gmail.com>
2024-10-24 00:52:49 +0530

    index.html updated updated

--- externalcss.js
+++ externalcss.js
@@ -5,9 +5,9 @@ metaveri.setAttribute("name", "yandex-verification");
 metaveri.setAttribute("content", "6cad077691947e7f");
 head.appendChild(metaveri);
 // meta verification end
+
 // google icon
 const googleicon = document.createElement("link");
-
 googleicon.setAttribute(
   "href",
   "https://fonts.googleapis.com/icon?family=Material+Icons"
@@ -27,6 +27,7 @@ googleapi3.setAttribute(
 );
 googleapi3.setAttribute("rel", "stylesheet");
 head.appendChild(googleapi3);
+
 /* <link
 href="https://fonts.googleapis.com/css2?family=Playwrite+CU:wght@100..400&display=swap"
 rel="stylesheet"
@@ -99,7 +100,6 @@ head.appendChild(gapi2);
   href="https://fonts.googleapis.com/css2?family=Playpen+Sans:wght@100..800&display=swap"
   rel="stylesheet"
 /> */
-
 // bootstrap font end
 
 // favicon start
@@ -136,25 +136,20 @@ fav512.setAttribute("type", "image/png");
 fav512.setAttribute("sizes", "512x512");
 fav512.href = "/assets/favicon_io/android-chrome-512x512.png";
 head.appendChild(fav512);
-
 // favicon end
-
 // bootstrap js script start
 const bootstrapScript = document.createElement("script");
 bootstrapScript.src =
   "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js";
-
 bootstrapScript.setAttribute(
   "integrity",
   "sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
 );
 bootstrapScript.setAttribute("crossorigin", "anonymous");
 head.appendChild(bootstrapScript);
-
 // bootstrap js script end
 
 // analytics
-
 const scriptrs = document.createElement("script");
 scriptrs.async = true;
 scriptrs.src = "https://www.googletagmanager.com/gtag/js?id=G-C4M8ZRW6HS";
