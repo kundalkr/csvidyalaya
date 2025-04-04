e9c7f0e154053200372c9509c58a6ccbfb0d7434
kundal kumar <kundalsingh960@gmail.com>
2025-04-04 14:55:55 +0530

    done

--- js/src/alljs.js
+++ js/src/alljs.js
@@ -5,10 +5,18 @@ external_css.src = "/js/src/externalcss.min.js";
 external_css.setAttribute("type", "module");
 headu.appendChild(external_css);
 
+if (window.location.pathname.startsWith("/os/scheduling_algorithms/")) {
+  let commonthings = document.createElement("script");
+  commonthings.src = "/os/scheduling_algorithms/commonthings.min.js";
+  commonthings.setAttribute("type", "module");
+  headu.appendChild(commonthings);
+}
+
 let header_footer = document.createElement("script");
 header_footer.src = "/js/src/hdr_ftr.min.js";
 header_footer.setAttribute("type", "module");
 header_footer.async = !0;
+// header_footer.defer = true;
 headu.appendChild(header_footer);
 
 window.onload = function () {
@@ -19,12 +27,6 @@ window.onload = function () {
   commonforall.async = !0;
   bodu.appendChild(commonforall);
 
-  if (window.location.pathname.startsWith("/os/scheduling_algorithms/")) {
-    let commonthings = document.createElement("script");
-    commonthings.src = "/os/scheduling_algorithms/commonthings.min.js";
-    commonthings.setAttribute("type", "module");
-    headu.appendChild(commonthings);
-  }
   if (
     window.location.hostname !== "localhost" &&
     window.location.hostname !== "127.0.0.1"
@@ -35,9 +37,19 @@ window.onload = function () {
     extra_tags.async = !0;
     bodu.appendChild(extra_tags);
   }
+  
 };
 
-
+if (
+  window.location.pathname.startsWith(
+    "/DBMS/DBMS_topics/relational_algebra/projection_operation.html"
+  )
+) {
+  let aa_value = document.createElement("script");
+  aa_value.src = "DBMS/js/aa_value.min.js";
+  aa_value.setAttribute("type", "module");
+  headu.appendChild(aa_value);
+}
 
 function c() {
   document.getElementById("sidebar").style.display = "block";
