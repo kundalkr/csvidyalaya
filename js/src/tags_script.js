91879b5c5be0fef5220b3530298475fb3b24cec3
kundal <kundal5335@gmail.com>
2025-05-10 01:08:46 +0530

    thing is done

--- js/src/tags_script.js
+++ js/src/tags_script.js
@@ -1,20 +1,8 @@
 import {add_head} from "/js/src/utilities.min.js";
-// let headr = document.querySelector("head");
 let scriptrs = document.createElement("script");
 scriptrs.async = !0;
 scriptrs.src = "https://www.googletagmanager.com/gtag/js?id=G-C4M8ZRW6HS";
 add_head(scriptrs);
-let jsscript = document.createElement("script");
-jsscript.type = "text/javascript";
-jsscript.text = `
-   window.dataLayer = window.dataLayer || [];
-   function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-C4M8ZRW6HS');
-`;
-add_head(jsscript);
-
 let metaveri = document.createElement("meta");
 metaveri.setAttribute("name", "yandex-verification");
 metaveri.setAttribute("content", "6cad077691947e7f");
