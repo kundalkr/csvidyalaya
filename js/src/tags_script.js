8ad3c2840466ee9543b4ac55971f9c84f8c47929
kundal <kundal5335@gmail.com>
2025-05-13 11:46:15 +0530

    thing is done

--- js/src/tags_script.js
+++ js/src/tags_script.js
@@ -1,9 +1,23 @@
 import {add_head} from "/js/src/utilities.min.js";
 
-let scriptrs = document.createElement("script");
-scriptrs.async = !0;
-scriptrs.src = "https://www.googletagmanager.com/gtag/js?id=G-C4M8ZRW6HS";
-add_head(scriptrs);
+// google analytics tags
+let script1 = document.createElement("script");
+script1.async = !0;
+script1.src = "https://www.googletagmanager.com/gtag/js?id=G-C4M8ZRW6HS";
+add_head(script1);
+
+let script2 = document.createElement("script");
+
+script2.innerHTML=`  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-C4M8ZRW6HS');`
+  add_head(script2);
+
+// google analytics tags end 
+
+
 
 
 
